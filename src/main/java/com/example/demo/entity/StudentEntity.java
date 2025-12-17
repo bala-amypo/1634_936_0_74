c4ebaba897db2850966ff0bdec260f4d32148475
bala-amypo <bala@amypo.in>
2025-12-17 08:28:36 +0000

    User commit

--- src/main/java/com/example/demo/entity/StudentEntity.java
+++ src/main/java/com/example/demo/entity/StudentEntity.java
@@ -1,37 +1,39 @@
 package com.example.demo.entity;
 
-
-
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import java.util.Date;
 
 @Entity
+
 public class StudentEntity{
-@Id
-@GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
 private Integer id;
 private String name;
 private String email;
 private String password;
 private Date created;
-public Date getCreated (){
+public Date getCreated(){
     return created;
 }
-public void setCreated(Date created) {
+public void setCreated(Date created){
     this.created=created;
 }
-public void getId() {
+
+public Integer getId(){
     return id;
 }
+
 public void setId(Integer id){
     this.id=id;
 }
-public String getname() {
+public String getName(){
     return name;
 }
-public void setName(String name) {
+public void setName(String Name){
     this.name=name;
 }
 public String getEmail(){
@@ -43,18 +45,18 @@ public void setEmail(String email){
 public String getPassword(){
     return password;
 }
-public void setPassword(String password){
+public void setPassword(String Password){
     this.password=password;
 }
 
-public StudentEntity(Integer Id, String name, String email, String password, Date created) {
+public StudentEntity(Integer Id, String name,String email,String password,Date created){
     this.id=id;
     this.name=name;
     this.email=email;
     this.password=password;
     this.created=created;
+    
 }
+
+               
 }
-
-
-

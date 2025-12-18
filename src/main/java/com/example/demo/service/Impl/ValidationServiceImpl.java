423f3f0ecd462f4b50fe3292c9c4282db44e8c11
bala-amypo <bala@amypo.in>
2025-12-18 08:51:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/Impl/ValidationServiceImpl.java
@@ -0,0 +1,41 @@
+package com.example.demo.service.Impl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.example.demo.entity.StudentEntity;
+import com.example.demo.repository.StudentRepo;
+import com.example.demo.service.ValidationService;
+import java.util.List;
+@Service
+public class ValidationServiceImpl implements ValidationService {
+    @Autowired StudentRepo student;
+    //save()
+    //findAll()
+    //findById()
+    //deleteById();
+    //existByid();
+        public StudentEntity postData(StudentEntity stu){
+            return student.save(stu);
+        }   
+        @Override 
+        public List<StudentEntity>getAllData(){
+            return student.findAll();
+        }   
+        @Override
+        public String DeleteData(int id){
+                    student.deleteById(id);
+                    return "Deleted Successfully";
+        }
+        @Override
+        public StudentEntity getData(int id){
+            return student.findById(id).orElse(null);
+        }
+        @Override
+        public StudentEntity updateData(int id,StudentEntity entity){
+            if (student.existsById(id)){
+                entity.setId(id);
+                 return student.save(entity);
+            }
+            return null;
+           
+        }
+}
